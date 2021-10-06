763b777aec9cdf7d6b5f525b7c6f092fcee6a481
Bakirov Ildar <bakirovildar2011@mail.ru>
2021-10-06 19:14:19 +0500

    Морской бой мини игра

--- chapter_1/hello_world/code.js
+++ chapter_1/hello_world/code.js
@@ -1,17 +1,27 @@
-function getRandomBoolean() {
-    return Boolean(Math.round(Math.random()));
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+while (isSunk==false) {
+    guess=prompt('Готовься,целься, стреляй  (выберите цифру от 0 до 6):');
+    if (guess<0 || guess>6) {
+        alert('Вы ввели некорректные данные');
+    }
+    else {
+        guesses = guesses+1;
+        if (guess == location1 || guess==location2 || guess==location3) {
+            hits = hits + 1;
+            if (hits==3) {
+                isSunk=true;
+                alert('Корабль уничтожен');
+            }
+        }
+    }
 }
+var stats='Вы сделали' + guesses + 'ходов, чтобы потопить корабль. Что означает Ваша точность стрельбы была'+(3/guesses);
+alert(stats);
 
-//const randomBoolean = getRandomBoolean();
-// if (randomBoolean) {
-//     var name='Ildar,Zarina';
-//     alert(name.split(',')[1])
-//     //alert(name.substring(2,3));
-// } else {
-//     alert('false');
-// }
-let randomBoolean=true
-while (randomBoolean===true) {
-    console.log('хоть нима');
-    randomBoolean=getRandomBoolean();
-}
+
