dbf4495dbf50f3166d37eb39f6b3118168d96f7a
Bakirov Ildar <bakirovildar2011@mail.ru>
2021-10-07 11:27:39 +0500

    Морской бой мини игра Версия 1.0

--- chapter_1/hello_world/code.js
+++ chapter_1/hello_world/code.js
@@ -1,27 +1,35 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
-while (isSunk==false) {
-    guess=prompt('Готовься,целься, стреляй  (выберите цифру от 0 до 6):');
-    if (guess<0 || guess>6) {
-        alert('Вы ввели некорректные данные');
-    }
-    else {
-        guesses = guesses+1;
-        if (guess == location1 || guess==location2 || guess==location3) {
-            hits = hits + 1;
-            if (hits==3) {
-                isSunk=true;
-                alert('Корабль уничтожен');
-            }
+var randomLoc = Math.floor(Math.random() * 5)
+var location1 = randomLoc
+var location2 = location1 + 1
+var location3 = location2 + 1
+var isHit1 = false
+var isHit2 = false
+var isHit3 = false
+var guess
+var guesses = 0
+var hits = 0
+var isSunk = false
+while (isSunk === false) {
+    guess = prompt('Напишите от 0 до 6')
+    if (guess < 0 || guess > 6) {
+        alert('Некорректные данные')
+    } else {
+        guesses = guesses + 1
+        if (guess === location1 && !isHit1
+            || guess === location2 && !isHit2
+            || guess === location3 && !isHit3) {
+            alert('Попал в цель')
+            isHit1 = guess === location1
+            isHit2 = guess === location2
+            isHit3 = guess === location3
+            hits = hits + 1
+        } else {
+            alert('Не попал')
+        }
+        if (hits === 3) {
+            isSunk = true
+            alert('Вы выиграли')
         }
     }
+
 }
-var stats='Вы сделали' + guesses + 'ходов, чтобы потопить корабль. Что означает Ваша точность стрельбы была'+(3/guesses);
-alert(stats);
-
-
