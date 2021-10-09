fc0de78243792e3d9876909ca4c7099af7e77b4b
Bakirov Ildar <bakirovildar2011@mail.ru>
2021-10-09 23:13:53 +0500

    Генерирование фраз. Работа с массивами

--- chapter_1/hello_world/code.js
+++ chapter_1/hello_world/code.js
@@ -1,12 +1,11 @@
-var balance = 10500;
-var cameraOn = true;
-function steal(balance, amount) {
-    cameraOn = false;
-    if (amount < balance) {
-        balance = balance - amount;
-    }
-    return amount;
-    cameraOn = true;
+function generate() {
+    var slova1 = ['Привет','Здравствуйте', 'Добрый день']
+    var slova2 = ['Как дела?','Как поживаете?', 'Как оно?']
+    var slova3 = ['Меня зовут Димон','Меня зовут Костян', 'Меня зовут Гена']
+
+    var gener1 = Math.floor(Math.random()* slova1.length)
+    var gener2 = Math.floor(Math.random()* slova2.length)
+    var gener3 = Math.floor(Math.random()* slova3.length)
+    alert(slova1[gener1]+' '+slova2[gener2]+ ' '+slova3[gener3])
 }
-var amount = steal(balance, 1250);
-alert("Criminal: you stole " + amount + "!");
+generate()
