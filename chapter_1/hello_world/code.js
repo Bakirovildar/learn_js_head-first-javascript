4e7864a31168c45789046921fadd996604b6edd4
Bakirov Ildar <bakirovildar2011@mail.ru>
2021-10-13 23:43:24 +0500

    Массивы. indexOf and incLudes

--- chapter_1/hello_world/code.js
+++ chapter_1/hello_world/code.js
@@ -1,13 +1,27 @@
-var scores = [60, 50, 60, 58, 54, 54,
-    58, 50, 52, 54, 48, 69,
-    34, 55, 51, 52, 44, 51,
-    69, 64, 66, 55, 52, 61,
-    46, 31, 57, 52, 44, 18,
-    41, 53, 55, 61, 51, 44];
+var famaly = ['mather', 'father', 'sister', 'brother']
+var homr = famaly.concat()
+    console.log(homr.indexOf('father'))
+    console.log(homr.indexOf('fathe'))
+    console.log(homr.includes('fathe'))
+    console.log(homr.includes('father'))
 
-var output;
-var i = 0;
-while (i<scores.length) {
-    console.log(i)
-    i = i+1
-}
+// var chery = {
+//     make: 'chevrolet',
+//     model: 'Cruze',
+//     year: 2015,
+//     color: 'grey',
+//     passengers: 2,
+//     convertible: false,
+//     mileage: 119000
+// };
+//
+// function prequal(chery) {
+//     if (chery.year<2015) {
+//         return false
+//     }
+//     else if (chery.mileage<118000) {
+//         return false
+//     }
+//     return true
+// }
+// console.log(prequal(chery))
