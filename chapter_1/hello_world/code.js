1812c74ef5482d12ad7a25f6b86b50f8604d8247
Bakirov Ildar <bakirovildar2011@mail.ru>
2021-10-23 19:09:36 +0500

    DOM Измененмие текста и цвета строки

--- chapter_1/hello_world/code.js
+++ chapter_1/hello_world/code.js
@@ -1,27 +1,6 @@
-var famaly = ['mather', 'father', 'sister', 'brother']
-var homr = famaly.concat()
-    console.log(homr.indexOf('father'))
-    console.log(homr.indexOf('fathe'))
-    console.log(homr.includes('fathe'))
-    console.log(homr.includes('father'))
-
-// var chery = {
-//     make: 'chevrolet',
-//     model: 'Cruze',
-//     year: 2015,
-//     color: 'grey',
-//     passengers: 2,
-//     convertible: false,
-//     mileage: 119000
-// };
-//
-// function prequal(chery) {
-//     if (chery.year<2015) {
-//         return false
-//     }
-//     else if (chery.mileage<118000) {
-//         return false
-//     }
-//     return true
-// }
-// console.log(prequal(chery))
+function add() {
+    var home = document.getElementById('code1')
+    home.innerHTML = 'Bakirov Ildar'
+    home.setAttribute('class', 'RedText')
+}
+window.onload = add
